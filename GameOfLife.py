4a898fd19a62fbbd37e40461a23c1b831cdb2156
rndmustafa <rndmustafa@gmail.com>
2016-10-09 17:23:13 -0400

    2D list is used to describe the grid instead of a dictionary

--- GameOfLife.py
+++ GameOfLife.py
@@ -7,27 +7,28 @@ def main():
     grid = user_input(grid)
     draw_grid(grid, row, col)
 
-    nex = input("n for next generation: ")
-    while nex == "n":
+    nex = input("hit enter for next generation: ")
+    while nex == "":
         grid = turn(grid, row, col)
         draw_grid(grid, row, col)
         nex = input("n for next generation: ")
+    
 
     
 
 def create_grid(row, col):
-    grid = {}
-    for x in range(1, row+1):
-        for y in range(1, col+1):
-            grid[(x, y)] = 0
-    
+    grid = []
+    for i in range (0, row):
+        grid.append([])
+        for j in range(0, col):
+            grid[i].append(0)
     
     return grid
 
 def draw_grid(grid, row, col):
-    for x in range(1, row+1):
-        for y in range(1, col+1):
-            print (grid[(x, y)], end = " ")
+    for i in range(0, row):
+        for j in range(0, col):
+            print (grid[i][j], end = " ")
         print()
     print()
 
@@ -41,7 +42,7 @@ def user_input(grid):
             place = place.split()
             row = int(place[0])
             col = int(place[1])
-            grid[(row, col)] = 1
+            grid[row-1][col-1] = 1
 
     return grid
 
@@ -50,23 +51,23 @@ def turn(grid, row, col):
     near = [(-1, -1), ( 0, -1), ( 1, -1), (-1,  0), ( 1,  0),
                   (-1,  1), ( 0,  1), ( 1,  1)]
 
-    for x in range(1, row+1):
-        for y in range(1, col+1):
+    for i in range(0, row):
+        for j in range(0, col):
             neighbor = 0
             for nx, ny in near:
                 try:
-                    if grid[(x+nx, y+ny)] == 1:
+                    if grid[i+nx][j+ny] == 1:
                         neighbor += 1
-                except KeyError:
+                except IndexError:
                     continue
-            if grid[(x, y)] == 0 and neighbor == 3:
-                new_grid[(x, y)] = 1
-            elif grid[(x, y)] == 1 and neighbor <= 1:
-                new_grid[(x, y)] = 0
-            elif grid[(x, y)] == 1 and neighbor >= 4:
-                new_grid[(x, y)] = 0
-            elif grid[(x, y)] == 1 and (neighbor == 2 or neighbor == 3):
-                new_grid[(x, y)] = 1
+            if grid[i][j] == 0 and neighbor == 3:
+                new_grid[i][j] = 1
+            elif grid[i][j] == 1 and neighbor <= 1:
+                new_grid[i][j] = 0
+            elif grid[i][j] == 1 and neighbor >= 4:
+                new_grid[i][j] = 0
+            elif grid[i][j] == 1 and (neighbor == 2 or neighbor == 3):
+                new_grid[i][j] = 1
 
     return new_grid
 
